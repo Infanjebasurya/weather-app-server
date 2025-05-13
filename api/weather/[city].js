1d0c7cbee32be6bc0dfea70cb707e392ddd66f9d
Infant Jeba Surya <infantjebasurya@gmail.com>
2025-05-13 08:13:05 +0000

    add CORS headers

--- api/weather/[city].js
+++ api/weather/[city].js
@@ -2,6 +2,15 @@ const API_KEY = process.env.OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export default async function handler(req, res) {
+   // Add CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*'); // or your exact frontend URL
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // respond to preflight request
+  }
+  
   const {
     query: { city },
     method,
